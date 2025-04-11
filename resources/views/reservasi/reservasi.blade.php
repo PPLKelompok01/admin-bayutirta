56cb1a027fa6bfea4bf2cdc6099e8a43aab7bc2e
FaishalD <faishald76@gmail.com>
2025-04-11 20:09:19 +0700

    git commit -m "PBI-4 <membuat komponen reservasi>"

---
+++ resources/views/reservasi/reservasi.blade.php
@@ -0,0 +1,38 @@
+@extends('main')
+@section('content')
+<div class="dashboard-admin">
+    <div class="container-fluid g-0">
+        <div class="row">
+            <div class="col-s-9">
+                <div class="content">
+                    <div class="titleWarp">
+                        <div class="layananTitle">Reservasi</div>
+                    </div>
+
+
+                    <div class="content-order">
+                        <h5 class="font-jakarta"></h5>
+                        <div class="service">
+                            <div class="time">
+                                <p>hola</p>
+                                <h5>hola</h5>
+                            </div>
+                            <div class="desc">
+                                <div class="type">
+                                    <p>Tipe handphone</p>
+                                    <h5>hola</h5>
+                                </div>
+                                <div class="status">
+                                    <p>Status Layanan</p>
+                                    <h5>hola</h5>
+                                </div>
+                                <button id="konfirmasi" type="button" class="btn confirm" data-bs-toggle="modal">Beri Konfirmasi</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
