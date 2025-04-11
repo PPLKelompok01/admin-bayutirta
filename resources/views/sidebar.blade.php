f32eda0d739f046cab63bb18254ec8b508a06aa9
Aaliyastenya <aaliyastenya@student.telkomuniversity.ac.id>
2025-04-11 19:38:13 +0700

    git commit -m "PBI-33 <Menambahkan komponen pada bagian sidebar>"

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -3,7 +3,7 @@
     <div class="side">
         <div class="logo d-flex w-100 align-items-center gap-3">
             <img class="logo-1" src="/img/Vector.png" alt="Logo" />
-            Bayu Tirta
+            Bayu Tirta Handphone
         </div>
         <div class="divider w-100"></div>
         <div class="menu w-100 gap-2">
@@ -43,9 +43,9 @@
                 Ulasan
             </a>
         </div>
-    
-        
-    
+
+
+
         <div class="acc d-flex position-relative w-100">
             <div class="role d-flex flex-column w-100 align-items-center position-absolute bottom-0 start-0">
                 <div class="position-absolute top-0 start-50 translate-middle">
@@ -53,7 +53,7 @@
                 </div>
                 <h6> {{ auth()->user()->name }} </h6>
                 <p> {{ auth()->user()->role }} </p>
-    
+
             </div>
         </div>
     </div>
