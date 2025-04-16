5382201da3281ab35073d36e807a7a6a498a5852
devissvtr <lkdevi16@gmail.com>
2025-04-16 15:46:03 +0700

    git commit -m "PBI-25 <add edit form>"

--- resources/views/artikel/editArtikel.blade.php
+++ resources/views/artikel/editArtikel.blade.php
@@ -6,9 +6,73 @@
         <div class="info-container">
             @if (isset($detail))
             <form class="info" action="/artikel/editArtikel/{{$detail->id_artikel}}" method="POST" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <h4>Detail Informasi Artikel</h4>
+            <div className="row align-items-center">
+                <label for="formFile" class="form-label">
+                    Upload Thumbnail Artikel (Jika ingin diubah)<span class="req">*</span>
+                </label>
+                <input class="form1 form-control" type="file" name="foto" id="formFile">
+            </div>
+
+            <div className="row align-items-center">
+                <label for="formFile" class="form-label">
+                    Judul Artikel<span class="req">*</span>
+                </label>
+                <input
+                    name="judul"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Tulis Judul Artikel"
+                    value="{{$detail->judul}}"
+                    required>
+            </div>
+            <div className="row align-items-center">
+                <label for="formFile" class="form-label">
+                    Nama Penulis<span class="req">*</span>
+                </label>
+                <input
+                    name="nama_penulis"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Tulis Nama Penulis"
+                    value="{{$detail->nama_penulis}}"
+                    required>
+            </div>
+            <div className="row align-items-center">
+                <label for="formFile" class="form-label">
+                    Title Penulis<span class="req">*</span>
+                </label>
+                <input
+                    name="title_penulis"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Tulis Title Penulis"
+                    value="{{$detail->title_penulis}}"
+                    required>
+            </div>
+            <div class="row align-items-center">
+                <label for="exampleFormControlTextarea1" class="form-label">
+                    Isi Artikel<span class="req">*</span>
+                </label>
+                <textarea
+                    name="isi"
+                    class="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    required>{{$detail->isi}}</textarea>
+            </div>
+            <div class="buttonGroup">
+                <a href="/artikel" class="btn btn btn-outline-danger" role="button">
+                    Batal
+                </a>
+                <button type="submit" class="btn btn-outline-success">
+                    Simpan</button>
+            </div>
         </form>
+
         @else
-            <form class="info" action="addArtikel" method="POST" enctype="multipart/form-data">
+        <form class="info" action="addArtikel" method="POST" enctype="multipart/form-data">
                     {{ csrf_field() }}
                     <h4>Detail Informasi Artikel</h4>
                 <div className="row align-items-center">
