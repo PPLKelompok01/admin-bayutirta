f6df15c00b1435b953518378c1e7af9d7d18c8ca
FaishalD <faishald76@gmail.com>
2025-04-28 11:02:32 +0700

    git commit -m "PBI-8 <make catalog lowongan appearance>"

--- resources/views/lowongan/lowongan.blade.php
+++ resources/views/lowongan/lowongan.blade.php
@@ -8,6 +8,24 @@
             Tambah lowongan
         </a>
     </div>
+
+    @if (isset($lowongan)AND $lowongan->count()>0)
+    <div class="card-list">
+        @foreach ($lowongan as $item)
+        <div class="card" style="width: 18rem;">
+            @if ($item->foto)
+            <img src="{{ url('images/lowongan/'.$item->foto) }}" style="height: 300px; object-fit: cover">
+            @endif
+            <div class="card-body">
+                <h4>{{$item->judul}}</h4>
+                <p class="card-teks">{{$item->isi}}</p>
+
+            </div>
+        </div>
+        @endforeach
+    </div>
+
+    @else
     <div class="katalog">
         <div class="picture">
             <img src="/img/ALT 4.png" alt="noservice">
@@ -17,6 +35,7 @@
             <p>Buat dan atur lowongan yang bisa diakses pelangganmu!</p>
             <p>Klik button “Tambah lowongan” di atas kanan halaman ini</p>
         </div>
+        @endif
     </div>
 </div>
 @endsection
